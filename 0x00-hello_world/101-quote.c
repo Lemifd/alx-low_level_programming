e2adbe95025b13269563c4084920333a3e589c1a
Lemi <lemifedasa0@gmail.com>
2022-04-07 05:37:07 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - printsthe pharse and that piece of art is useful
  * " - Dora Korpar, 2015-10-19
- * * Return: 1 if the program works correcty, 0(zero) otherwise
+ * Return: 1 if the program works correcty, 0(zero) otherwise
  */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-		return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
