2662970ee88657f2134aeeef2895dc0b6aac6e1a
Lemi <lemifedasa0@gmail.com>
2022-04-07 01:53:34 -0700

    msg

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,10 @@
  *
  * Return: Always 0 (Success)
  */
+int write(int filedes, const char *buf, unsigned int nbyte); 
 int main(void)
 {
-        fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }
