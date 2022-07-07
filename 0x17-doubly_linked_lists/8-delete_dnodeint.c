38c18250f28b168eb3380d96883b3eec3574e0fd
Lemi <lemifedasa0@gmail.com>
2022-07-07 17:21:12 -0400

    recent updated

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,41 +1,42 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - delete node at index
- * @head: double pointer to head of node
- * @index: index to delete
- * Return: list with deleted node, 1 on success, -1 on fail
+ * delete_dnodeint_at_index - Deletes the node at given index of a list
+ * @head: Memory address of the linked list
+ * @index: Index of the list to search
+ *
+ * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-unsigned int count = 0;
-dlistint_t *temp = NULL;
+	dlistint_t *temp = *head, *prev = NULL;
+	unsigned int i = 0;
 
-if (head == NULL || *head == NULL)
-return (-1);
+	if (head == NULL || temp == NULL)
+		return (-1);
 
-temp = *head;
-if (index == 0)
-{
-*head = (*head)->next;
+	if (index == 0)
+	{
+		if (temp->next != NULL)
+			temp->next->prev = NULL;
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
 
-if (*head != NULL)
-(*head)->prev = NULL;
-free(temp);
-return (1);
-}
-while (temp->next != NULL)
-{
-if (count == index)
-{
-temp->next->prev = temp->prev;
-temp->prev->next = temp->next;
-free(temp);
-return (1);
-}
-temp = temp->next;
-count++;
-}
+	while (temp != NULL && i != index)
+	{
+		prev = temp;
+		temp = temp->next;
+		i++;
+	}
+	if (temp == NULL)
+		return (-1);
 
-return (-1);
+	prev->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = prev;
+	free(temp);
+
+	return (1);
 }
